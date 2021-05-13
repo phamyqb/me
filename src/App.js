52c4bd6841eb4344839085c9854f8693552b6878
Thomas Pham <thomas_pham@nfcguest.com>
2021-05-13 21:46:14 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,246 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import {
+	FaGlobe,
+	FaGraduationCap,
+	FaThumbsUp,
+	FaGrav,
+} from "react-icons/fa";
+import {
+	INFO,
+	EDUCATION,
+	SKILLS,
+	LANGUAGES,
+	INTERESTS,
+	EXPERIENCES,
+} from "./constants";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const personalInfo = () => {
+		return (
+			<section className="block border-box">
+				<h3>Personal Info</h3>
+				<ul className="mt-3 info">
+					<li>
+						<span>Birthdate</span> : {INFO.BIRTHDAY}
+					</li>
+					<li>
+						<span>Phone</span> : {INFO.PHONE}
+					</li>
+					<li>
+						<span>Email</span> : {INFO.EMAIL}
+					</li>
+					<li>
+						<span>Skype</span> : {INFO.SKYPE}
+					</li>
+					<li>
+						<span>Freelance</span> :{" "}
+						{INFO.FREELANCER ? "Available" : "Not Available"}
+					</li>
+					<li>
+						<span>Adress</span> : {INFO.ADDRESS}
+					</li>
+				</ul>
+			</section>
+		);
+	};
+
+	const education = () => {
+		return (
+			<section className="block border-box">
+				<h3>Education</h3>
+				<div className="block-icon">
+					<FaGraduationCap />
+				</div>
+
+				<div className="timeline mt-3">
+					{EDUCATION.map((edu, idx) => (
+						<div key={idx} className="time">
+							<div className="hgroup mb-3">
+								<h4>{edu.title}</h4>
+								<h6>{edu.time}</h6>
+							</div>
+							<p>{edu.description}</p>
+						</div>
+					))}
+				</div>
+			</section>
+		);
+	};
+
+	const skills = () => {
+		return (
+			<section className="block border-box">
+				<h3>Skills</h3>
+				<div className="block-icon">
+					<FaGrav />
+				</div>
+
+				<div className="skills mt-3">
+					<ul>
+						{Object.keys(SKILLS).map((key) => {
+							const value = SKILLS[key];
+							return (
+								<li key={key}>
+									<h4>{key}</h4>
+									<div className="rating">
+										{[...Array(5).keys()].map(
+											(key, index) => {
+												return (
+													<span
+														key={key}
+														className={
+															index + 1 > value
+																? "transparent"
+																: ""
+														}
+													></span>
+												);
+											}
+										)}
+									</div>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</section>
+		);
+	};
+
+	const languages = () => {
+		return (
+			<section className="block border-box">
+				<h3>Languages</h3>
+				<div className="block-icon">
+					<FaGlobe />
+				</div>
+
+				<div className="skills mt-3">
+					<ul>
+						{Object.keys(LANGUAGES).map((key) => {
+							const value = LANGUAGES[key];
+							return (
+								<li key={key}>
+									<h4>{key}</h4>
+									<div className="rating">
+										{[...Array(5).keys()].map(
+											(key, index) => {
+												return (
+													<span
+														key={key}
+														className={
+															index + 1 > value
+																? "transparent"
+																: ""
+														}
+													></span>
+												);
+											}
+										)}
+									</div>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</section>
+		);
+	};
+
+	const interests = () => {
+		return (
+			<section className="block border-box rm-b">
+				<h3>Interests</h3>
+				<div className="block-icon">
+					<FaThumbsUp />
+				</div>
+				<div className="hobbies mt-4">
+					<ul>
+						{INTERESTS.map((i, idx) => (
+							<Fragment key={idx}>
+								<li>
+									<span>{i.icon}</span>
+									<h6>{i.label}</h6>
+								</li>
+							</Fragment>
+						))}
+					</ul>
+				</div>
+			</section>
+		);
+	};
+
+	const summary = () => {
+		return (
+			<section>
+				<div className="block border-box">
+					<h3>Summary</h3>
+					<p className="mt-3 summary">
+						Having 5 years of experiences in Web/ Mobile
+						development. I'm a creative, hardworking, enthusiastic,
+						ambitious and able to work under high pressure.
+						<br></br>I'm passionate about new technology and
+						professional work environment. My focus in on trying to
+						learn as much as possible and doing my best in order to
+						accomplish assigned task, supporting my team members,
+						research and apply new technologies effectively.
+					</p>
+				</div>
+			</section>
+		);
+	};
+
+	const workExperiences = () => {
+		return (
+			<section className="block">
+				<h3>Work Experiences</h3>
+				<div className="work-experiences mt-4">
+					{EXPERIENCES.map((exp, idx) => (
+						<div key={idx} class="listing-content">
+							<h6 className="exp-date">{exp.date}</h6>
+							<h6 className="exp-title">{exp.title}</h6>
+							<p className="tag">{exp.responsibility}</p>
+							<p className="mt-3">{exp.description}</p>
+							<div>
+								{exp.techs.map((t, ind) => (
+									<span className="tag" key={ind}>
+										{t}
+									</span>
+								))}
+							</div>
+						</div>
+					))}
+				</div>
+			</section>
+		);
+	};
+
+	return (
+		<div className="main">
+			<header className="header">
+				<div className="name">
+					<h2>Y Pham</h2>
+					<span>Senior Software Developer</span>
+				</div>
+				<div className="avatar"></div>
+			</header>
+
+			<main className="d-flex">
+				<div className="left f-1">
+					{personalInfo()}
+					{education()}
+					{skills()}
+					{languages()}
+					{interests()}
+				</div>
+
+				<div className="right f-2">
+					{summary()}
+					{workExperiences()}
+				</div>
+			</main>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,167 @@
+import { FaMusic, FaGamepad, FaPlane, FaCookie } from "react-icons/fa";
+
+export const INFO = {
+	BIRTHDAY: "10-05-1995",
+	EMAIL: "phamyqb@gmail.com",
+	SKYPE: "phamyqb",
+	PHONE: "0337785816",
+	FREELANCER: true,
+	ADDRESS: "Da Nang, Viet Nam",
+};
+
+export const EDUCATION = [
+	{
+		title: "Da Nang College of Technology - PN Vietnam",
+		time: "Jul 2014 – Nov 2016",
+		description: "Bachelor of Engineering",
+	},
+];
+
+export const SKILLS = {
+	"HTML/CSS": 4,
+	JQuery: 4,
+	React: 4,
+	Angular: 4,
+	"React Native": 4,
+	"Flutter": 3,
+	"C#, ASP.NET, .NET Core": 4,
+	Python: 3,
+	"MS SQL Server, MySQL": 4,
+	Teamwork: 5,
+	Git: 5,
+};
+
+export const LANGUAGES = {
+	Vietnamese: 5,
+	English: 3,
+};
+
+export const INTERESTS = [
+	{
+		label: "Music",
+		icon: <FaMusic />,
+	},
+	{
+		label: "Game",
+		icon: <FaGamepad />,
+	},
+	{
+		label: "Travel",
+		icon: <FaPlane />,
+	},
+	{
+		label: "Cooking",
+		icon: <FaCookie />,
+	},
+];
+
+export const EXPERIENCES = [
+	{
+		title: "SkoolNet (Singapore) - Orient Software",
+		date: "Dec 2020 - Present",
+		description:
+			"This is a system of Singapore client, They had a existed their system that built for 5 years ago, using React and React Native. " +
+			"Belong to the education majo and allows their customers config by individual school. School can manage the posts, announcement and parents, children of the parents that studied in" +
+			"the school. " +
+			"Currently I'm working for new features and updating their system." +
+			"",
+		techs: [
+			"React",
+			"React Native",
+			"GraphQL",
+			"S3 Bucket",
+			"Golang",
+			"MySQL",
+		],
+		responsibility: "FE Team Leader",
+	},
+	{
+		title: "Enici (Samurai Security - Japan) - Orient Software",
+		date: "Jun 2020 - Nov 2020",
+		description:
+			"Coming from a company of Japan, they want to develop the internal communication channel during the Covid-19 pandemic. " +
+			"This application allows users to send the message with frames and give receive award point to exchange gif from their store." +
+			"The system was built on both website and mobile application using Firebase.",
+		techs: ["React", "React Native", "Firebase", "Google Cloud"],
+		responsibility: "Team Leader",
+	},
+	{
+		title: "Holtel Service (Vingroup) - Orient Software",
+		date: "May 2019 - Jun 2020",
+		description:
+			"Coming from Vingroup company, they want to build a part the internal service to finding hotel for comparing the price and services. " +
+			"Provide for their customer a service to search the holtels and booking. " +
+			"The system was built on web app using Angular 8.",
+		techs: ["Angular", "Azure DevOps", ".NET Core", "Kafka", "MySQL"],
+		responsibility: "Developer",
+	},
+	{
+		title: "Airmine (Australia) - Orient Software",
+		date: "Jan 2019 - May 2019",
+		description:
+			"Airmine Project is the responsive for both web and mobile application using .NET core and React Native to manage healthcare information and display the weather using current location. " +
+			"Mobile application allows users to send the current location with temparature to the system, tracking and manage the data to report. " +
+			"The web app is an admin site for the administrator to get the detailed, and overview of the data.",
+		techs: ["React", "React Native", ".NET Core", "SQL Server"],
+		responsibility: "Developer",
+	},
+	{
+		title: "Dryer (Japan) - Orient Software",
+		date: "Aug 2018 - Dec 2018",
+		description:
+			"Dryer Project is the responsive web application development for display and operator of 10 sensors and manage them. Start, Stop, Pause, and export data, " +
+			"display as real time. The website allows to drag & drop the components on layout, turn on and turn off the sensor.",
+		techs: ["React", "Redux Saga", "Python", "PostgreSQL", "Google Cloud"],
+		responsibility: "Developer",
+	},
+	{
+		title: "Inital Image Search (Japan) - Orient Software",
+		date: "May 2018 - Aug 2018",
+		description:
+			"This is the responsive web application development for image searching model, " +
+			"display result finding by AI. The website allows to drag & drop the components on layout, turn on AI for image search." +
+			"Using the image as input and process to search the same original image as output",
+		techs: ["React", "Redux Saga", "Python", "PostgreSQL", "Google Cloud"],
+		responsibility: "Developer",
+	},
+	{
+		title: "STAR - Survey Tracking (Malaysia) - FPT Software",
+		date: "Jan 2018 - May 2018",
+		description:
+			"The purpose of the STAR System is to automate and improve the Contractor Performance Evaluation (CPE), and replace the existing SRM Module for CPE. " +
+			"The cumbersome processes lead to incomplete vendor reviews and inaccurate vendor performance ratings. Ratings were not used systematically in formal feedback reviews. " +
+			"Ratings, reviews and vendor feedback were not analyzed for prescriptive measures.",
+		techs: ["Angular", "KendoUI", ".NET", "Sql Server"],
+		responsibility: "Developer",
+	},
+	{
+		title: "Hotel Booking (Australia) - Success Software Service (HCM)",
+		date: "Jun 2017 - Dec 2017",
+		description:
+			"The objective of the project is to implement a web application to manage multiple sites which provide services to booking hotel, booking restaurant, air plan, etc..., " +
+			"User can create new site by using this application and using template that application provided or create new template for the site of the user.",
+		techs: ["Angular", "JQuery", ".NET", "Sql Server"],
+		responsibility: "Developer",
+	},
+	{
+		title: "Investor (Australia) - Success Software Service (HCM)",
+		date: "Jan 2017 - Jun 2017",
+		description:
+			"The objective of the project is to make solution and develop a web application to help the company manage their investors, " +
+			"investment contracts as well as profit. Implement enhancements and fixing remaining defects which hosts multiple payment forms that integrate with external payment process Point and Pay. " +
+			"It’s has many roles for user (such as: Administrator, investors…) and authenticated by the role of user logged in. " +
+			"Each role of user has different permission on this system (View by fields, page and data type…)." +
+			"Administrator can export csv and pdf file to report by using this system..",
+		techs: [".NET", "JQuery", "Sql Server"],
+		responsibility: "Developer",
+	},
+	{
+		title: "House Provider (USA) - Success Software Service (HCM)",
+		date: "Sep 2016 - Jan 2017",
+		description:
+			"The objective of the project is to make solution and develop a web system provides the services for customer to find (address, counties, hotel…) in America. " +
+			"And offer user find services easier. This application accepts user login by location or IP address.",
+		techs: [".NET MVC", "JQuery", "Sql Server"],
+		responsibility: "Developer",
+	},
+];
